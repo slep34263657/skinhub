6460c1fae9b8e180d016d435418b4cc30891f85c
slep <sl3pgg@gmail.com>
2026-01-14 16:33:30 +0000

    a

--- generate-skins-json.js
+++ generate-skins-json.js
@@ -1,34 +1,10 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const SKINS_DIR = path.join(__dirname, "skins");
-const OUTPUT = path.join(__dirname, "skins.json");
+const skinsDir = path.join(__dirname, 'skins');
+const skins = fs.readdirSync(skinsDir).filter(name => {
+  const fullPath = path.join(skinsDir, name);
+  return fs.statSync(fullPath).isDirectory();
+}).sort();
 
-function walk(dir, base) {
-  let results = [];
-
-  for (const file of fs.readdirSync(dir)) {
-    const fullPath = path.join(dir, file);
-    const stat = fs.statSync(fullPath);
-
-    if (stat.isDirectory()) {
-      results = results.concat(walk(fullPath, base));
-    } else {
-      results.push(path.relative(base, fullPath).replace(/\\/g, "/"));
-    }
-  }
-
-  return results;
-}
-
-const skins = {};
-
-for (const skinName of fs.readdirSync(SKINS_DIR)) {
-  const skinPath = path.join(SKINS_DIR, skinName);
-  if (!fs.statSync(skinPath).isDirectory()) continue;
-
-  skins[skinName] = walk(skinPath, skinPath);
-}
-
-fs.writeFileSync(OUTPUT, JSON.stringify(skins, null, 2));
-console.log("skins.json generated");
+console.log('Found skins:', skins);
